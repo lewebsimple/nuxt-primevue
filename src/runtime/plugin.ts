c05dba479781e9cfd1d45cfa622b7083e24ee62d
Pascal Martineau <pascal@lewebsimple.ca>
2022-10-07 10:52:17 -0400

    fix: Missing directives

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -7,6 +7,10 @@ import ToastService from "primevue/toastservice";
 import ConfirmDialog from "primevue/confirmdialog";
 import ConfirmPopup from "primevue/confirmpopup";
 import Toast from "primevue/toast";
+
+import BadgeDirective from "primevue/badgedirective";
+import Ripple from "primevue/ripple";
+import StyleClass from "primevue/styleclass";
 import Tooltip from "primevue/tooltip";
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -14,8 +18,13 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(ConfirmationService);
   nuxtApp.vueApp.use(DialogService);
   nuxtApp.vueApp.use(ToastService);
+
   nuxtApp.vueApp.component("ConfirmDialog", ConfirmDialog);
   nuxtApp.vueApp.component("ConfirmPopup", ConfirmPopup);
   nuxtApp.vueApp.component("Toast", Toast);
+
+  nuxtApp.vueApp.directive("badge", BadgeDirective);
+  nuxtApp.vueApp.directive("ripple", Ripple);
+  nuxtApp.vueApp.directive("styleclass", StyleClass);
   nuxtApp.vueApp.directive("tooltip", Tooltip);
 });
